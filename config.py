88300fc60e58c7f7cc97d74ac1c3009bb59b5f01
Vincent.W <974834300@qq.com>
2018-07-31 09:32:04 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 import absl.flags as flags
 import torch
-import torch.backends.cudnn as cudnn
+from main import train,dev
+from preproc import preproc
+from absl import app
 
 '''
 The content of this file is mostly copied from https://github.com/HKUST-KnowComp/R-Net/blob/master/config.py
@@ -9,17 +11,17 @@ The content of this file is mostly copied from https://github.com/HKUST-KnowComp
 
 home = os.path.expanduser(".")
 target_dir = 'data'
-train_file = os.path.join(home, target_dir, "squad", "train-v1.1.json")
-dev_file = os.path.join(home, target_dir, "squad", "dev-v1.1.json")
-test_file = os.path.join(home, target_dir, "squad", "dev-v1.1.json")
+train_file = os.path.join(home, target_dir, "squad", "train-v2.0.json")
+dev_file = os.path.join(home, target_dir, "squad", "dev-v2.0.json")
+test_file = os.path.join(home, target_dir, "squad", "dev-v2.0.json")
 glove_word_file = os.path.join(home, target_dir, "glove", "glove.840B.300d.txt")
 
 event_dir = "log"
 save_dir = "model"
 answer_dir = "log"
-train_record_file = os.path.join(target_dir, "train.json")
-dev_record_file = os.path.join(target_dir, "dev.json")
-test_record_file = os.path.join(target_dir, "test.json")
+train_token_file = os.path.join(target_dir, "train.json")
+dev_token_file = os.path.join(target_dir, "dev.json")
+test_token_file = os.path.join(target_dir, "test.json")
 word_emb_file = os.path.join(target_dir, "word_emb.json")
 char_emb_file = os.path.join(target_dir, "char_emb.json")
 train_eval = os.path.join(target_dir, "train_eval.json")
@@ -31,6 +33,10 @@ word2idx_file = os.path.join(target_dir, "word2idx.json")
 char2idx_file = os.path.join(target_dir, "char2idx.json")
 answer_file = os.path.join(answer_dir, "answer.json")
 
+train_logi_file = os.path.join(target_dir, "train_logi.json")
+dev_logi_file = os.path.join(target_dir, "dev_logi.json")
+test_logi_file = os.path.join(target_dir, "test_logi.json")
+
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 if not os.path.exists(event_dir):
@@ -41,7 +47,7 @@ if not os.path.exists(answer_dir):
     os.makedirs(answer_dir)
 
 flags.DEFINE_string("mode", "dev", "preprocess/train/debug/dev/test")
-flags.DEFINE_boolean("finetune", True, "finetune the model")
+flags.DEFINE_boolean("finetune", False, "finetune the model")
 
 flags.DEFINE_string("target_dir", target_dir, "")
 flags.DEFINE_string("event_dir", event_dir, "")
@@ -51,9 +57,9 @@ flags.DEFINE_string("dev_file", dev_file, "")
 flags.DEFINE_string("test_file", test_file, "")
 flags.DEFINE_string("glove_word_file", glove_word_file, "")
 
-flags.DEFINE_string("train_record_file", train_record_file, "")
-flags.DEFINE_string("dev_record_file", dev_record_file, "")
-flags.DEFINE_string("test_record_file", test_record_file, "")
+flags.DEFINE_string("train_token_file", train_token_file, "")
+flags.DEFINE_string("dev_token_file", dev_token_file, "")
+flags.DEFINE_string("test_token_file", test_token_file, "")
 flags.DEFINE_string("word_emb_file", word_emb_file, "")
 flags.DEFINE_string("char_emb_file", char_emb_file, "")
 flags.DEFINE_string("train_eval_file", train_eval, "")
@@ -76,8 +82,8 @@ flags.DEFINE_integer("emb_dim", 100, "Dimension of connectors of each layer")
 flags.DEFINE_integer("head_num", 6, "Number of heads in multi-head attention")
 flags.DEFINE_integer("attention_map_dim", 64, "MultiHeadAttentnion middle dimension")
 flags.DEFINE_integer("kernel_size", 3, "kernel size of CNN")
-flags.DEFINE_integer("encoder_block_num", 17, "Number of blocks in the model encoder layer")
-flags.DEFINE_integer("model_block_num", 5, "Number of blocks in the model encoder layer")
+flags.DEFINE_integer("encoder_block_num", 5, "Number of blocks in the model encoder layer")
+flags.DEFINE_integer("model_block_num", 17, "Number of blocks in the model encoder layer")
 
 
 flags.DEFINE_integer("para_limit", 400, "Limit length for paragraph")
@@ -95,7 +101,7 @@ flags.DEFINE_list("bucket_range", [40, 401, 40], "the range of bucket")
 
 flags.DEFINE_integer("batch_size", 32, "Batch size")
 flags.DEFINE_integer("epochs", 10, "Number of epochs")
-flags.DEFINE_integer("checkpoint", 500, "checkpoint to save and evaluate the model")
+flags.DEFINE_integer("checkpoint", 1000, "checkpoint to save and evaluate the model")
 flags.DEFINE_integer("period", 100, "period to save batch loss")
 flags.DEFINE_integer("val_batch_size", 20, "Number of batches to evaluate the model")
 flags.DEFINE_float("dropout", 0.1, "Dropout prob across the layers")
@@ -103,7 +109,7 @@ flags.DEFINE_float("dropout_char", 0.05, "Dropout prob across the layers")
 flags.DEFINE_float("grad_clip", 5.0, "Global Norm gradient clipping rate")
 flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
 flags.DEFINE_float("decay", 0.9999, "Exponential moving average decay")
-# flags.DEFINE_float("l2_norm", 3e-7, "L2 norm scale")
+flags.DEFINE_float("L2_norm", 3e-7, "L2 norm scale")
 # flags.DEFINE_integer("hidden", 96, "Hidden size")
 flags.DEFINE_integer("early_stop", 10, "Checkpoints for early stop")
 
@@ -121,9 +127,38 @@ fasttext_file = os.path.join(home, target_dir, "fasttext", "wiki-news-300d-1M.ve
 flags.DEFINE_string("fasttext_file", fasttext_file, "Fasttext word embedding")
 flags.DEFINE_boolean("fasttext", False, "Whether to use fasttext")
 
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+cpu = torch.device("cpu")
 config = flags.FLAGS
 
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
-cpu = torch.device("cpu")
+def run(_):
+    if config.mode == "train":
+        train(config,device)
+    elif config.mode == "preprocess":
+        preproc(config)
+        train(config,device)
 
-cudnn.enabled = False
+
+    elif config.mode == "debug":
+        config.epochs = 1
+        config.batch_size = 32
+        config.val_batch_size = 20
+        config.checkpoint = 1
+        config.period = 1
+        train(config,device)
+    elif config.mode == "test":
+        pass
+    elif config.mode == "dev":
+        dev(config,device)
+    else:
+        print("Unknown mode")
+        exit(0)
+
+if __name__ == '__main__':
+    app.run(run)
+
+
+
+
+
+
